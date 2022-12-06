6a9dd0557fb2f74f0e6ece02251887aa51a0cf4a
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-12-06 15:22:37 +0100

    set up form and even handlers

--- src/components/AddProductToCart/index.tsx
+++ src/components/AddProductToCart/index.tsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import { ReactComponent as CartIcon } from "../../assets/icons/icon-cart.svg";
 import styles from "./AddProductToCart.module.scss";
 
 const AddProductToCart = (): JSX.Element => {
+  const [numberOfProducts, setNumberOfProducts] = useState<number>(1);
+
+  const handleClickIncrease = () => {
+    setNumberOfProducts((prev) => prev + 1);
+  };
+
+  const handleClickDecrease = () => {
+    if (numberOfProducts > 1) {
+      setNumberOfProducts((prev) => prev - 1);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumberOfProducts(parseInt(e.target.value, 10) || 1);
+  };
+
   return (
     <form className={styles.addToCartContainer}>
       <div className={styles.input}>
-        <button type="button">-</button>
-        <p>0</p>
-        <button type="button">+</button>
+        <button type="button" onClick={handleClickDecrease}>
+          -
+        </button>
+        <input
+          type="number"
+          aria-label="quantity"
+          onChange={handleChange}
+          value={numberOfProducts}
+        />
+        <button type="button" onClick={handleClickIncrease}>
+          +
+        </button>
       </div>
       <button type="submit" className={styles.addToCartButton}>
         <CartIcon className={styles.cartIcon} />
