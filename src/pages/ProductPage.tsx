efed344e80920768bc41b38d80e4a0e262f3c86b
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-12-16 20:03:19 +0100

    add props

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -5,6 +5,7 @@ import ProductImages from "../components/ProductImages";
 import ProductDescription from "../components/ProductDescription";
 import ProductPrice from "../components/ProductPrice";
 import AddProductToCart from "../components/AddProductToCart";
+import styles from "./ProductPage.module.scss";
 
 const ProductPage = (): JSX.Element => {
   const { sneakerId } = useParams();
@@ -29,18 +30,26 @@ const ProductPage = (): JSX.Element => {
   return (
     <>
       {sneaker && (
-        <div>
-          <ProductImages images={sneaker.url} />
-          <ProductDescription
-            brand={sneaker.brand}
-            name={sneaker.name}
-            description={sneaker.description}
-          />
-          <ProductPrice
-            price={sneaker.price}
-            discountPrice={sneaker.discountPrice}
-          />
-          <AddProductToCart />
+        <div className={styles.productPageContainer}>
+          <div className={styles.imagesContainer}>
+            <ProductImages images={sneaker.url} />
+          </div>
+          <div className={styles.descriptioAndFormContainer}>
+            <ProductDescription
+              brand={sneaker.brand}
+              name={sneaker.name}
+              description={sneaker.description}
+            />
+            <ProductPrice
+              price={sneaker.price}
+              discountPrice={sneaker.discountPrice}
+            />
+            <AddProductToCart
+              thumbnailImage={sneaker.url.imageThumbnail1}
+              name={sneaker.name}
+              discountPrice={sneaker.discountPrice}
+            />
+          </div>
         </div>
       )}
     </>
