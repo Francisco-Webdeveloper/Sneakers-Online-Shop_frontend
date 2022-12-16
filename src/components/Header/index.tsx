3b73ea86686c2f1fad62245dc5d4dfb69e8d7957
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-12-16 20:02:58 +0100

    add check cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,30 +4,47 @@ import cart from "../../assets/icons/icon-cart.svg";
 import logo from "../../assets/icons/logo.svg";
 import avatar from "../../assets/images/image-avatar.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Cart from "../Cart";
 
 const Header = () => {
+  const [openCart, setOpenCart] = useState(false);
+
+  const handleOpenCart = () => setOpenCart(true);
+
+  const handleCloseCart = () => setOpenCart(false);
+
   return (
-    <header className={styles.headerContainer}>
-      <div className={styles.menuAndLogoContainer}>
-        <img className={styles.mobileMenu} src={menu} alt="menu" />
-        <Link to={"/"}>
-          <img src={logo} alt="logo" />
-        </Link>
-        <nav>
-          <ul className={styles.desktopMenu}>
-            <li>Collections</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </div>
-      <div className={styles.cartAndAvatarContainer}>
-        <img src={cart} alt="cart" />
-        <img className={styles.avatarContainer} src={avatar} alt="avatar" />
-      </div>
-    </header>
+    <>
+      <header className={styles.headerContainer}>
+        <div className={styles.menuAndLogoContainer}>
+          <img className={styles.mobileMenu} src={menu} alt="menu" />
+          <Link to={"/"}>
+            <img src={logo} alt="logo" />
+          </Link>
+          <nav>
+            <ul className={styles.desktopMenu}>
+              <li>Collections</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.cartAndAvatarContainer}>
+          <button
+            type="submit"
+            onClick={handleOpenCart}
+            className={styles.cartButton}
+          >
+            <img src={cart} alt="cart" />
+          </button>
+          <img className={styles.avatarContainer} src={avatar} alt="avatar" />
+        </div>
+      </header>
+      {openCart && <Cart openCart={openCart} onHide={handleCloseCart} />}
+    </>
   );
 };
 
